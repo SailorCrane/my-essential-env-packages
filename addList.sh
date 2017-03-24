56314eb34751f385481fbfd8426c3e7b536a7048
Crane <215379951@qq.com>
2017-03-24 23:51:52 +0800

    all is ok v1.0 version can run, but not perfect

--- addList.sh
+++ addList.sh
@@ -1,7 +1,9 @@
 pkgs=$(./filter-package.sh)
 
+echo -n "set(CPACK_DEBIAN_PACKAGE_DEPENDS \""
 for pkgName in $pkgs ; do
-    echo "list(APPEND CPACK_DEBIAN_PACKAGE_DEPENDS \"$pkgName\")"
+    echo -n  "$pkgName, "
 done
+    echo "\" )"
 
 #list(APPEND CPACK_DEBIAN_PACKAGE_DEPENDS "vim-gnome")      # 使用list append, 避免参数挤在一起.
